1700c7cc276f49411ffcde2767d42bf138912bd3
Unknown <bushmango@gmail.com>
2021-11-04 11:30:02 -0500

    basic skeleton

--- pages/impulse-sub-pixel.tsx
+++ pages/impulse-sub-pixel.tsx
@@ -10,12 +10,9 @@ const ImpulseSubPixelPage: NextPage = () => {
       <HeadTitle title="Impulse Sub-Pixel Editing Software" />
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Keep Flying Studios LLC Placeholder</h1>
+        <h1 className={styles.title}>Impulse Sub-Pixel</h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <p className={styles.description}></p>
 
         <div>Codename Impulse Sub-Pixel</div>
       </main>
