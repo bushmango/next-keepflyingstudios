5dca599c73497519d597ecfdbd3ae5bd7f408df8
Unknown <bushmango@gmail.com>
2021-11-23 13:37:28 -0600

    more sign in fixes

--- lib/sessionUtil.ts
+++ lib/sessionUtil.ts
@@ -1,5 +1,8 @@
 import { Session } from 'next-auth'
 
 export const getUsername = (session: Session) => {
+  if (!session) {
+    return 'no-session'
+  }
   return '' + (session?.user?.name || session?.user?.email || 'unknown')
 }
--- pages/account/login-app-impulse.tsx
+++ pages/account/login-app-impulse.tsx
@@ -2,17 +2,21 @@ import type { NextPage } from 'next'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
+import { ErrorBoundary } from '../../components/core/ErrorBoundary'
 import { Footer } from '../../components/layout/Footer'
 import { HeadTitle } from '../../components/layout/HeadTitle'
 import { getQueryStringParameterByName } from '../../lib/getQueryString'
+import { getUsername } from '../../lib/sessionUtil'
 import styles from '../../styles/Home.module.css'
 
 function LoggedInComponent() {
   const { data: session } = useSession()
   if (session) {
+    let username = getUsername(session)
+
     return (
       <>
-        Signed in as {session?.user?.email} <br />
+        Signed in as {username} <br />
         <button onClick={() => signOut()}>Sign out</button>
         {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
       </>
@@ -38,7 +42,9 @@ const LoginPage: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Login (Impulse Sub-Pixel Editor)</h1>
-        <LoggedInComponent />
+        <ErrorBoundary>
+          <LoggedInComponent />
+        </ErrorBoundary>
         <br />
         {url && <Link href={url}>Back to Editor</Link>}
       </main>
