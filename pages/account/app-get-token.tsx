f3f6039e9da932612754badb2543525c6c0c475d
Unknown <bushmango@gmail.com>
2021-12-10 15:24:21 -0600

    swap pages

--- pages/account/app-get-token.tsx
+++ pages/account/app-get-token.tsx
@@ -1,75 +1,35 @@
-/* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
-
-// interface ITokenData {
-//   user_id: string
-//   email: string
-//   user_access_token: string
-// }
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const session = await getSession(context)
-
-//   if (session && session.user) {
-//     // const data = await prismaClient.user.findUnique({
-//     //   where: {
-//     //     email: session.user.email,
-//     //   },
-//     //   select: {
-//     //     id: true,
-//     //     email: true,
-//     //     custom_access_token: true,
-//     //   },
-//     // })
-//     return {
-//       props: {
-//         data: {
-//           user_id: session.user_id,
-//           user_email: session.user?.email || '',
-//           user_name: session.user?.name || '',
-//           user_access_token: session.user_access_token,
-//         },
-//       },
-//     }
-//   }
-
-//   return { props: {} }
-// }
-
-// const AppGetTokenPage: InferGetServerSidePropsType<typeof getServerSideProps> =
-//   (props: { data?: ITokenData }) => {
+import { useEffect } from 'react'
 
 const AppGetTokenPage: NextPage = () => {
   const { status, data } = useSession()
-  const [tokenData, setTokenData] = useState('')
   let session = data
   useEffect(() => {
     if (window && window.top) {
+      let tokenData = ''
       if (status === 'authenticated') {
+        tokenData = 'loading-session'
         if (session) {
-          setTokenData(
-            JSON.stringify(
-              {
-                user_id: session.user_id,
-                user_email: session.user?.email || '',
-                user_name: session.user?.name || '',
-                user_access_token: session.user_access_token,
-              },
-              null,
-              2,
-            ),
-          )
+          tokenData = JSON.stringify({
+            // user_id: session.user_id,
+            user_email: session.user?.email || '',
+            user_name: session.user?.name || '',
+            // user_access_token: session.user_access_token,
+          })
         }
+        window.top.postMessage('tokens:' + tokenData, '*')
+      }
+      if (status === 'unauthenticated') {
+        window.top.postMessage('tokens:' + tokenData, '*')
       }
     }
   }, [status, session])
 
   return (
     <div>
-      Getting token data... {'' + status}
-      <pre>{tokenData}</pre>
+      Getting api keys... {'' + status}
+      {/* <pre>{JSON.stringify(props.data, null, 2)}</pre> */}
     </div>
   )
 }
--- pages/account/get-api-keys.tsx
+++ pages/account/get-api-keys.tsx
@@ -1,37 +1,38 @@
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-const GetApiKeysPage: NextPage = () => {
+const GetApiKeys: NextPage = () => {
   const { status, data } = useSession()
+  const [tokenData, setTokenData] = useState('')
   let session = data
   useEffect(() => {
     if (window && window.top) {
-      let tokenData = ''
       if (status === 'authenticated') {
-        tokenData = 'loading-session'
         if (session) {
-          tokenData = JSON.stringify({
-            user_id: session.user_id,
-            user_email: session.user?.email || '',
-            user_name: session.user?.name || '',
-            user_access_token: session.user_access_token,
-          })
+          setTokenData(
+            JSON.stringify(
+              {
+                user_id: session.user_id,
+                user_email: session.user?.email || '',
+                user_name: session.user?.name || '',
+                user_access_token: session.user_access_token,
+              },
+              null,
+              2,
+            ),
+          )
         }
-        window.top.postMessage('tokens:' + tokenData, '*')
-      }
-      if (status === 'unauthenticated') {
-        window.top.postMessage('tokens:' + tokenData, '*')
       }
     }
   }, [status, session])
 
   return (
     <div>
-      Getting api keys... {'' + status}
-      {/* <pre>{JSON.stringify(props.data, null, 2)}</pre> */}
+      Getting token data... {'' + status}
+      <pre>{tokenData}</pre>
     </div>
   )
 }
 
-export default GetApiKeysPage
+export default GetApiKeys
